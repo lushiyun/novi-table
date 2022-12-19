8ea0bcaa527024b4ef2feaa28df57665fae3ff87
Shiyun <lushiyun@outlook.com>
2022-12-19 10:36:58 -0500

    Construct Table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import persons from '../../data/persons.json';
+
 interface Person {
+	id: string;
 	name: string;
 	email: string;
 	age: number;
@@ -10,8 +13,48 @@ interface Person {
 	location: string;
 }
 
-const Table = () => {
-	return <div>Table</div>;
+const personTitles = [
+	'Name',
+	'Email',
+	'Age',
+	'Occupation',
+	'Number of Cats',
+	'Daily Water Consumption',
+	'Location',
+];
+
+const Table: React.FC<{
+	onRowClick: (id: string) => void;
+}> = ({ onRowClick }) => {
+	const selectRow = (id: string) => {
+		onRowClick(id);
+	};
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					{personTitles.map((title) => (
+						<th>{title}</th>
+					))}
+				</tr>
+			</thead>
+
+			<tbody>
+				{persons.map((person) => (
+					<tr onClick={() => selectRow(person.id)}>
+						<td>{person.name}</td>
+						<td>{person.email}</td>
+						<td>{person.age}</td>
+						<td>{person.occupation}</td>
+						<td>{person.number_of_cats}</td>
+						<td>{person.daily_water_consumption}</td>
+						<td>{person.location}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 };
 
 export { Table };
